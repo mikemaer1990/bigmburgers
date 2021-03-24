6e29774635a2f388ab066c0560ee351d8e35cc2d
Michael Maertens <30853323+mikemaer1990@users.noreply.github.com>
2021-03-24 19:18:42 +0800

    add career/events page finish menu page

--- static/js/app.js
+++ static/js/app.js
@@ -6,6 +6,30 @@ hamburger.addEventListener("click", () => {
     navBarLinks.classList.toggle("open");
 });
 
+// const eventList = [{
+//         title: "Durham Region Classic Mustang Club",
+//         info: "Annual Club Event. There will be door prizes, DJ music and great fun as per usual. Come out for a burger and show some club spirit!",
+//         date: "Sunday June 23rd, 2019 4:00pm",
+//         img: "./static/imgs/mustang.png",
+//         url: "https://drcmc.com"
+//     },
+//     {
+//         title: "Corvettes of Durham",
+//         info: "Corvette Night Social Event",
+//         date: "Wednesday July 17th, 2019 5:30pm",
+//         img: "./static/imgs/corvette.png",
+//         url: "http://www.corvettesofdurham.ca/"
+//     }
+// ];
+
+// for (let i = 0; i < eventList.length; i++) {
+//     const parent = document.querySelector('.events-list');
+//     let newDiv = document.createElement('div');
+//     newDiv.innerHTML = `<div class="event-wrap"><img class="event-img" src="${eventList[i].img}"><p class="event-title">${eventList[i].title}</p><p class="event-info">${eventList[i].info}</p><p class="event-date">${eventList[i].date}</p><br><a href="${eventList[i].url}>Website</a></div>`
+//     newDiv.setAttribute('class', 'event-item');
+//     parent.appendChild(newDiv)
+// }
+
 
 // jquery
 $('.close-x').click(function () {
