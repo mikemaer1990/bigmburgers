fd6b7018b89862020d2d3227e45578d86d6073d2
Michael Maertens <30853323+mikemaer1990@users.noreply.github.com>
2021-03-26 18:01:08 +0800

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,7 @@ const links = document.querySelectorAll(".navBarLinks li");
 
 hamburger.addEventListener("click", () => {
     navBarLinks.classList.toggle("open");
+    hamburger.classList.toggle("change");
 });
 
 
